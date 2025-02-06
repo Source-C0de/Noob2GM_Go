5376ddfb411ba7187a38c675f7b80f6afc8bca08
Fahmim Mohammod Shahriar <63873694+Source-C0de@users.noreply.github.com>
2025-02-06 23:00:27 +0300

    add new file

--- Problem Solving Go/February/Feb_01022501.go
+++ Problem Solving Go/February/Feb_01022501.go
@@ -3,16 +3,15 @@ package main
 import fmt
 
 func isArraySpecial(nums []int) bool {
+
     for i:=1;i<len(nums);i++ {
         if nums[i]%2 == 0 && nums[i-1]%2==1 {
             continue;
-        }
-        if nums[i-1]%2 == 0 && nums[i]%2==1 {
+        }if nums[i-1]%2 == 0 && nums[i]%2==1 {
             continue;
         }else{
             return false;
-        }
-        
+        } 
     }
     return true;
 }
